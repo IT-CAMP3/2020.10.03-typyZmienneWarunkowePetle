55b6d0a887ca0513814d8a08114e17de3ee476b8
Mateusz <bmatteo@interia.pl>
2020-10-04 12:36:57 +0200

    commit2

---
+++ src/Main11.java
@@ -0,0 +1,13 @@
+public class Main11 {
+    public static void main(String[] args) {
+        /*for(int i = -9; i < 40; i=i+2) {
+            System.out.println(i);
+        }*/
+
+        int i = -9;
+        while (i < 40) {
+            System.out.println(i);
+            i=i+2;
+        }
+    }
+}
